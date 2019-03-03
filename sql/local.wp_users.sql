646607f7a3a9d256962f1b7934230bc8124644c8
Nate Finch <nate@thedigitalring.com>
2019-03-03 13:50:59 -0600

    initial update

---
+++ sql/local.wp_users.sql
@@ -0,0 +1,5 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_users` VALUES
+(1,"n8finch","$P$BIIL3ck4vXwJ9cMDKnytlBFKpjyeB61","n8finch","nate@thedigitalring.com","","2019-02-07 21:36:01","",0,"n8finch");
