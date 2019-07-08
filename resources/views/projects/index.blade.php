bb6d6f90a0ad6e0351111bc8339dbc2bf40206a0
Nate Finch <nate@thedigitalring.com>
2019-07-08 17:09:16 -0500

    routes etc.

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -11,7 +11,9 @@
 
     <ul>
         @foreach ($projects as $project)
-            <li>{{ $project->title }}</li>
+            <li>
+                <a href="{{$project->path()}}"/>{{ $project->title }}</a>
+            </li>
         @endforeach
     </ul>
 </body>
