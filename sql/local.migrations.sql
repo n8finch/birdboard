eaf710a59ed52d5135effb8b3504e0341285ee24
Nate Finch <nate@thedigitalring.com>
2019-07-08 17:01:53 -0500

    updates to tests for projects

---
+++ sql/local.migrations.sql
@@ -0,0 +1,7 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `migrations` VALUES
+(1,"2014_10_12_000000_create_users_table",1),
+(2,"2014_10_12_100000_create_password_resets_table",1),
+(3,"2019_03_04_125705_create_projects_table",1);
---
+++ sql/local.projects-schema.sql
@@ -0,0 +1,11 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+
+CREATE TABLE `projects` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
